9f149294b72e9d467ad08f9c9c38a6748b509bde
Vera Lakmaker <vera.lakmaker@gmail.com>
2025-03-31 17:33:42 +0200

    Updated url again

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -8,7 +8,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://vlakmaker.github.io',
-  baseUrl: '/XueCodex/',
+  baseUrl: '/',
 
   organizationName: 'vlakmaker', // Your GitHub username
   projectName: 'XueCodex',       // Your repo name
